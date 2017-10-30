35b8767bfdbeefc8c797d76479131c4a624fe4c2
Kyle Suss <susskyle@gmail.com>
2017-10-30 14:20:15 -0600

    feat: first styled component setup

--- src/components/checklist.js
+++ src/components/checklist.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const SyledChecklist = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`
 
 const Checklist = ({ tasks }) => (
-  <div>The checklist</div>
+  <SyledChecklist>
+    <div>The checklist</div>
+  </SyledChecklist>
 );
 
 export default Checklist;
--- src/styles/base.js
+++ src/styles/base.js
@@ -2,6 +2,12 @@ import * as colors from './colors';
 import * as fonts from './fonts';
 
 const base = `
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
   body {
     font-family: ${fonts.LATO};
     background: ${colors.WHITE};
